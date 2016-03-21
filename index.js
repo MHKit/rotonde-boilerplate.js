c45c22b1f05526d1af5592817547885b50f83b77
squirelo <valentin.squirelo@gmail.com>
2016-03-21 13:03:10 +0100

    add myo

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 'use strict'
 
-const client = require('rotonde-client/node/rotonde-client')('ws://127.0.0.1:4224');
+const _ = require('lodash');
+
+const client = require('rotonde-client/node/rotonde-client')('ws://bionicodevkit.local:4224');
+
+// ===================
+// Code pour la reception d'un evenement MYO_POSE_EDGE
+
+client.eventHandlers.attach('MYO_POSE_EDGE', (event) => {
+  console.log(event.identifier);
+
+  // Inserer ici le code de control de la main
+});
+
+// ===================
 
 client.onReady(() => {
   console.log('Connected');
