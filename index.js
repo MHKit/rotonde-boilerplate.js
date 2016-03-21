ddd9263c364c20e7029279f01cdde6d66a193a1c
squirelo <valentin.squirelo@gmail.com>
2016-03-21 13:04:07 +0100

    add hand control

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ client.eventHandlers.attach('MYO_POSE_EDGE', (event) => {
   console.log(event.identifier);
 
   // Inserer ici le code de control de la main
+  if (event.data.pose == 'wave_right') {
+    client.sendAction('HAND_FINGERS', { // même structure que le champs data du paquet envoyé via l'extension chrome
+      "fingers": [
+        {"position": 0, "speed": 1},
+        {"position": 0, "speed": 1},
+        {"position": 0, "speed": 1},
+        {"position": 0, "speed": 1},
+        {"position": 0, "speed": 1}
+      ]
+    });
+  } else {
+    client.sendAction('HAND_FINGERS', { // comme le cas précédent, sauf que les positions sont à 1
+      "fingers": [
+        {"position": 1, "speed": 1},
+        {"position": 1, "speed": 1},
+        {"position": 1, "speed": 1},
+        {"position": 1, "speed": 1},
+        {"position": 1, "speed": 1}
+      ]
+    });
+  }
 });
 
 // ===================
-
 client.onReady(() => {
   console.log('Connected');
 });
