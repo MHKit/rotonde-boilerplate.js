35331e072f4fbdbbe6fc3c4b4e6f03937735ce7e
Valentin Squirelo <valentin.squirelo@gmail.com>
2016-03-21 12:22:27 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const _ = require('lodash');
+
+const client = require('rotonde-client/node/rotonde-client')('ws://127.0.0.1:4224');
+
+client.onReady(() => {
+  console.log('Connected');
+});
+
+client.connect();
